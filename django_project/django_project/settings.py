2d05aafa452e62db5db3715cc95fba522f98dca0
Simon <sema.patyukov.84@mail.ru>
2025-05-30 00:17:35 +0300

    3.5Commit

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -98,10 +98,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Для collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'django_project/static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'django_project/static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
